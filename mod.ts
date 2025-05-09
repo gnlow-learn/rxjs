1c5210869beb4cc880c4d74eec4d61426d793732
Gnlow <gnlow@pm.me>
2025-05-09 16:04:12 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,17 @@
-import { of } from "https://esm.sh/rxjs@7.8.2"
+import {
+    of,
+    map,
+    groupBy,
+    mergeMap,
+    toArray,
+} from "https://esm.sh/rxjs@7.8.2"
 
-const a = of(1, 2)
+const a =
+    of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    .pipe(
+        map(x => x * 2),
+        groupBy(x => x > 10),
+        mergeMap(x => x.pipe(toArray())),
+    )
 
 a.subscribe(console.log)
